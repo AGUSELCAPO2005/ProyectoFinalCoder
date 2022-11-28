afc732c5a0c552e8eb7d88a37b004e6de79b5e06
Agustin Godoy <agustin.gaston5@gmail.com>
2022-11-28 17:49:15 -0300

    avance super basico con templates

--- proyectofinal/urls.py
+++ proyectofinal/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
+    path('academiadefutbol/', include('home.urls'))
 ]
